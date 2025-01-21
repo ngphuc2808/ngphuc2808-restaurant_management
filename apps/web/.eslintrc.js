e5740bac88888673764c3ed57d36c4c7e5cbdcc8
ngphuc2808 <ngphuc.2808@gmail.com>
2025-01-21 10:53:12 +0700

    feat: set up another lib for web

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,20 @@
 /** @type {import("eslint").Linter.Config} */
+
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: [
+    "@repo/eslint-config/next.js",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
+  },
+  plugins: ["@tanstack/query"],
+  rules: {
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/no-deprecated-options": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error",
+    "@tanstack/query/stable-query-client": "error",
   },
 };
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
